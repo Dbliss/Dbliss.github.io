753b85a5f3f71cb4bfb8a4d394f7c14999c03a3c
Dbliss <dillzdabomb@hotmail.com>
2025-11-20 15:14:13 +1100

    fixed config to run on github pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const repoName = process.env.GITHUB_REPOSITORY
-  ? process.env.GITHUB_REPOSITORY.split('/')[1]
-  : null
-const isUserOrOrgSite = repoName ? repoName.endsWith('.github.io') : false
-
 export default defineConfig({
   plugins: [vue()],
-  base: repoName && !isUserOrOrgSite ? `/${repoName}/` : '/',
+  base: '/', // for dbliss.github.io root; default is already '/'
 })
